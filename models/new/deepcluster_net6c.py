eee44429106439bf9b23f1575eff1d0f381622cb
xuji <xj112358@gmail.com>
2018-09-27 20:06:40 +0100

    .

--- models/new/deepcluster_net6c.py
+++ models/new/deepcluster_net6c.py
@@ -39,12 +39,15 @@ class DeepClusterNet6c(VGGNet):
         num_features = DeepClusterNet6c.cfg[-1][0]
 
         assert(input_sp_sz == 24)
+
+        """
         self.classifier = nn.Sequential(
             nn.Linear(num_features * 3 * 3, 4096),
             nn.ReLU(True)
         )
+        """
 
-        self.top_layer = nn.Linear(4096, out)
+        self.top_layer = nn.Linear(num_features * 3 * 3, out)
 
         self._initialize_weights()
 
@@ -54,8 +57,8 @@ class DeepClusterNet6c(VGGNet):
     def forward(self, x, penultimate=False):
         x = self.features(x)
 
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
+        #x = x.view(x.size(0), -1)
+        #x = self.classifier(x)
 
         # used by assess code
         if penultimate:
@@ -66,4 +69,5 @@ class DeepClusterNet6c(VGGNet):
         return x
 
 def deepcluster_net6c(sobel=False, out=None, input_sp_sz=None, input_ch=None):
+    assert(False)
     return DeepClusterNet6c(sobel, out, input_sp_sz, input_ch)
