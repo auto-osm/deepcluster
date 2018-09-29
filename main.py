e1b76b8d22dcbd4c2067be1a0ad9b23adbcc8dc6
xuji <xj112358@gmail.com>
2018-09-30 00:04:50 +0100

    .

--- main.py
+++ main.py
@@ -289,7 +289,7 @@ def main():
             train_dataset,
             batch_size=args.batch_sz,
             num_workers=args.workers,
-            #sampler=sampler,
+            sampler=sampler,
             pin_memory=True,
         )
 
