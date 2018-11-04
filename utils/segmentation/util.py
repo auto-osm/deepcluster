5ef79b07636445e659f4fa717e61eaed26f6301b
xuji <xj112358@gmail.com>
2018-11-04 00:54:28 +0000

    .

--- utils/segmentation/util.py
+++ utils/segmentation/util.py
@@ -11,17 +11,19 @@ def compute_spatial_features(args, dataloader, model, num_imgs):
       assert(len(tup) == 2)
       imgs, mask = tup
 
-    mask = mask.cpu().astype(np.bool)
+    mask = mask.cpu().numpy().astype(np.bool)
 
     with torch.no_grad():
       # penultimate = features
       x_out = model(imgs, penultimate=True).cpu().numpy().astype(np.float32)
 
     if i == 0:
+      assert(x_out.shape[1] == model.dlen)
       assert(x_out.shape[2] == args.input_sz and
              x_out.shape[3] == args.input_sz)
-      features = np.zeros((num_imgs, x_out.shape[1], args.input_sz, args.input_sz))
-      masks = np.zeros((num_imgs, args.input_sz, args.input_sz))
+      features = np.zeros((num_imgs, x_out.shape[1], args.input_sz,
+                           args.input_sz), dtype=np.float32)
+      masks = np.zeros((num_imgs, args.input_sz, args.input_sz), dtype=np.bool)
 
     if i < len(dataloader) - 1:
       features[i * args.batch_sz: (i + 1) * args.batch_sz] = x_out
